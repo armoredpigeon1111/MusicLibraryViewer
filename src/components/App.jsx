7b9b6ee2d896ae21b8ac06c2e720a7444ab274c4
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-09-09 16:12:27 -0400

    Added MusicViewer.jsx to get and display a list of songs and details from the MusicLibrary API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import MusicViewer from './MusicViewer/MusicViewer';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+           
+         }
     }
+
+
+
     render() { 
         return ( 
             <div>
+                <h1>Music Library Viewer</h1>
                 <MusicViewer />
             </div>
          );
--- src/components/MusicViewer/MusicViewer.jsx
+++ src/components/MusicViewer/MusicViewer.jsx
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import './MusicViewer.css'
 
 class MusicViewer extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            songs: []
+         }
     }
+
+    componentDidMount(){
+        this.getSongs();
+    }
+
+    async getSongs(){
+        let response = await axios.get('http://127.0.0.1:8000/music/');
+        console.log(response);
+        this.setState({
+            songs: response.data
+        });
+    }
+
     render() { 
         return ( 
-            <div>
-                
-            </div>
+            <table>
+                <thead>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                </thead>
+                {this.state.songs.map((song)=>{
+                    return(
+                        <tr>
+                            <td>{song.title}</td>
+                            <td>{song.artist}</td>
+                            <td>{song.album}</td>
+                            <td>{song.release_date}</td>
+                            <td>{song.genre}</td>
+                            <td><button>Delete</button></td>
+                            
+                        </tr>
+                            
+                        
+                    );
+                })}
+            </table>
          );
     }
 }
